2678dd888ff74de948d812f86eaa0f6887ecdc6e
shawA04 <aisha_shawish@mines.edu>
2026-05-02 23:29:28 -0600

    Added all the tables into the database, so everyone should be able to see thsoe when they set their search_path to our group number. Had to change some of the table creations in the schema.sql because the schema expected a wide format from the csv, but our csv's were in long format. Also had to adjust the constraints on snotel to allow for all the values and it had an extra row so I deleted that.

--- extract_stations.py
+++ extract_stations.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('snotel_all_stations.csv', 'r', newline='') as infile:
+with open('snowpack_data\\snotel_all_stations_test.csv', 'r', newline='') as infile:
     with open('stations.csv', 'w', newline='') as outfile:
         reader = csv.DictReader(infile)
         writer = csv.DictWriter(outfile, fieldnames=['station_id', 'station_name'])
